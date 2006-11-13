a1f0ad7efb1edc4bcc39d86b975a059bc2d35dfb
cerha <cerha>
2006-11-13 14:29:41 +0000

    *** empty log message ***

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
-# -*- coding: iso-8859-2 -*-
 """Pytis configuration for running a standalone application."""
 
 import getpass, sys
 
 def_dir = '/home/cerha/work/wiking/defs'
-help_dir =  '/home/cerha/work/asellus/pytis-help'
-icon_dir =  '/home/cerha/work/asellus/pytis/icons'
+help_dir =  '/home/cerha/work/pytis/help'
+icon_dir =  '/home/cerha/work/pytis/icons'
 application_name = 'Wiking'
 log_one_line_preferred = True
 dbuser = getpass.getuser()
