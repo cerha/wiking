156401121a9de4730778fb0fb19bfbfc2a5f3555
cerha <cerha>
2006-12-04 14:02:30 +0000

    *** empty log message ***

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,6 @@
 -- Wiking initial data --
 
-INSERT INTO languages (lang) VALUES ('en'); 
+INSERT INTO languages (lang) VALUES ('en');
 
 INSERT INTO modules (name, active) VALUES ('Content', 't');
 INSERT INTO modules (name, active) VALUES ('News', 't');
@@ -11,14 +11,14 @@ INSERT INTO modules (name, active) VALUES ('Stylesheets', 't');
 INSERT INTO modules (name, active) VALUES ('Themes', 't');
 INSERT INTO modules (name, active) VALUES ('Languages', 't');
 
-INSERT INTO _mapping (identifier, mod_id, published, ord) 
+INSERT INTO _mapping (identifier, mod_id, published, ord)
 VALUES ('index', 1, 't', 1);
 
-INSERT INTO _mapping (identifier, mod_id, published, ord) 
-VALUES ('css', 2, 't', NULL);
+INSERT INTO _mapping (identifier, mod_id, published, ord)
+VALUES ('css', 6, 't', NULL);
 
-INSERT INTO content (mapping_id, lang, title, content) 
-VALUES (1, 'en', 'Welcome', 
+INSERT INTO content (mapping_id, lang, title, content)
+VALUES (1, 'en', 'Welcome',
 	'Your new Wiking site has been succesfully set up.\n\n'||
 	'Enter the [/wmi Wiking Management Interface] to manage the content.');
 
