f81d583077991cf4e8a54bc622ba261a0250b696
cerha <cerha>
2006-12-11 20:58:28 +0000

    (Exporter._menu): Added accesskeys. (Exporter._content): Cosmetic change. (Exporter.ctrl): Changed WMI uri.  Also recognize the doc state. (Exporter._footer): Added `Accessibility Statement' link.

--- lib/wiking/export.py
+++ lib/wiking/export.py
@@ -65,6 +65,7 @@ class Exporter(lcg.HtmlExporter):
     def _menu(self, node):
         links = [_html.link(item.title() + (cur and hidden(' *') or ''),
                             self._node_uri(item),
+                            hotkey=(item.id() == 'index' and "1" or None),
                             cls=("navigation-link"+(cur and " current" or "")))
                  for item, cur in
                  [(item, self._is_current(node, item)) for item in node.menu()]]
@@ -73,8 +74,9 @@ class Exporter(lcg.HtmlExporter):
         else:
             skip_target = '#content-heading'
         skip_lnk = hidden(" (", _html.link(_("skip"), skip_target),")")
-        label = _html.strong(concat(_("Main navigation"), skip_lnk, ":"),
-                             cls='label')
+        l = _html.link(_("Main navigation"), None, name='main-navigation',
+                       hotkey="3")
+        label = _html.strong(concat(l, skip_lnk, ":"), cls='label')
         sep = " "+hidden("|")+"\n"
         skip_all = hidden(_html.link(_("Skip all repetitive content"),
                                      '#content-heading', hotkey="2"))
@@ -118,8 +120,8 @@ class Exporter(lcg.HtmlExporter):
 
     def _content(self, node):
         h = _html.h(_html.link(node.title(), None, name='content-heading'), 1)
-        return _html.div((h, super(Exporter, self)._content(node)),
-                         cls='node-id-'+node.id())
+        content = (h, super(Exporter, self)._content(node))
+        return _html.div(content, cls='node-id-'+node.id())
 
     def _clearing(self, node):
         return '&nbsp;'
@@ -127,6 +129,34 @@ class Exporter(lcg.HtmlExporter):
     def _bottom(self, node):
         return self._parts(node, ('wiking_bar', 'last_change', 'footer'))
                            
+    def _wiking_bar(self, node):
+        import wiking
+	result = (_html.hr(),)
+        ctrls = (hidden("["),)
+        if node.edit_label():
+            ctrls += (_html.link(node.edit_label(), "?action=edit"), "|")
+        if node.config().wmi:
+            ctrl = _html.link(_("Leave the Management Interface"), '/',
+                              hotkey="9")
+        elif node.config().doc:
+            ctrl = _html.link(_("Leave the Help System"), '/')
+        else:
+            ctrl = _html.link(_("Manage this site"), '/_wmi/',
+                              title=_("Enter the Wiking Management Interface"),
+                              hotkey="9")
+        ctrls += (ctrl, hidden("]"))
+        result += (_html.span(concat(ctrls, separator="\n"), cls="controls"),)
+        result += (_html.span(_("Powered by %s %s",
+                                _html.link("Wiking",
+                                           "http://www.freebsoft.org/wiking"),
+                                wiking.__version__)),)
+        return result
+        
+
+    def _last_change(self, node):
+        return None
+	return _("Last change: %s, %s")
+
     def _footer(self, node):
         links = [_html.link(label, uri, title=title) + ','
                  for label, uri, title in
@@ -146,35 +176,11 @@ class Exporter(lcg.HtmlExporter):
         contact = node.config().webmaster_addr
         return (_html.hr(),
                 p(_("This site conforms to the following standards:"), *links),
-                p(_("This site can be viewed in ANY browser.")),
+                p(_("This site can be viewed in ANY browser."),
+                  _html.link(_("Accessibility Statement"),
+                             '/_doc/accessibility?display=inline', hotkey='0')),
                 p(_("Contact:"), _html.link(contact, "mailto:"+contact)))
     
-    def _wiking_bar(self, node):
-        import wiking
-	result = (_html.hr(),)
-        ctrls = (hidden("["),)
-        if node.edit_label():
-            ctrls += (_html.link(node.edit_label(), "?action=edit"), "|")
-        if node.config().wmi:
-            ctrl = _html.link(_("Leave the Management Interface"), '/',
-                              hotkey="9")
-        else:
-            ctrl = _html.link(_("Manage this site"), '/wmi',
-                              title=_("Enter the Wiking Management Interface"),
-                              hotkey="9")
-        ctrls += (ctrl, hidden("]"))
-        result += (_html.span(concat(ctrls, separator="\n"), cls="controls"),)
-        result += (_html.span(_("Powered by %s %s",
-                                _html.link("Wiking",
-                                           "http://www.freebsoft.org/wiking"),
-                                wiking.__version__)),)
-        return result
-        
-
-    def _last_change(self, node):
-        return None
-	return _("Last change: %s, %s")
-
 
 def hidden(*text):
     return _html.span(text, cls="hidden")
