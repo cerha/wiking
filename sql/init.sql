60c9ad38eea83cec34b445914549edab6f577bba
cerha <cerha>
2006-12-22 23:19:40 +0000

    Stylesheet module number fixed.  WMI URI fixed.

--- sql/init.sql
+++ sql/init.sql
@@ -16,12 +16,12 @@ INSERT INTO _mapping (identifier, mod_id, published, ord)
 VALUES ('index', 1, 't', 1);
 
 INSERT INTO _mapping (identifier, mod_id, published, ord)
-VALUES ('css', 6, 't', NULL);
+VALUES ('css', 7, 't', NULL);
 
 INSERT INTO content (mapping_id, lang, title, content)
 VALUES (1, 'en', 'Welcome',
-	'Your new Wiking site has been succesfully set up.\n\n'||
-	'Enter the [/wmi Wiking Management Interface] to manage the content.');
+    'Your new Wiking site has been succesfully set up.\n\n' ||
+    'Enter the [/_wmi Wiking Management Interface] to manage the content.');
 
 INSERT INTO config (site_title) VALUES ('Wiking site');
 
