333270705dc18b0a7e40db71fd9127c7bf1705f7
cerha <cerha>
2007-04-02 11:43:15 +0000

    (MenuItem): Added support for item descriptions.

--- lib/wiking/util.py
+++ lib/wiking/util.py
@@ -200,13 +200,16 @@ class FileUpload(object):
 
 class MenuItem(object):
     """Menu item representation to be passed to 'Document.mknode()'."""
-    def __init__(self, id, title):
+    def __init__(self, id, title, descr=None):
         self._id = id
         self._title = title
+        self._descr = descr
     def id(self):
         return self._id
     def title(self):
         return self._title
+    def descr(self):
+        return self._descr
 
 
 class Panel(object):
