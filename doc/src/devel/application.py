1040b683809f98c61882c98853e919c5374630c5
cerha <cerha>
2007-09-07 09:03:51 +0000

    Include method arguments.

--- doc/src/devel/application.py
+++ doc/src/devel/application.py
@@ -1,4 +1,4 @@
-import lcg
+import lcg, inspect
 
 class Reader(lcg.Reader):
     """Generate Wiking API documentation out of Python docstrings."""
@@ -13,9 +13,12 @@ class Reader(lcg.Reader):
         #           and v.__module__ == 'wiking.application']
         #modules.sort()
         content = parser.parse(Application.__doc__) + \
-                  [lcg.TableOfContents(title="Application methods:")] + \
-                  [lcg.Section(title=name+'()', content=parser.parse(attr.__doc__))
-                   for name, attr in Application.__dict__.items()
-                   if not name.startswith('_') and not name.startswith('action_') \
-                   and callable(attr) and attr.__doc__]
+                  [lcg.TableOfContents(title="Application methods:")]
+        for name, method in Application.__dict__.items():
+            if name.startswith('_') or name.startswith('action_') \
+                   or not callable(method) or not method.__doc__:
+                continue
+            args, varargs, varkw, defaults = inspect.getargspec(method)
+            title = name + inspect.formatargspec(args[1:], varargs, varkw, defaults)
+            content.append(lcg.Section(title=title, content=parser.parse(method.__doc__)))
         return content
