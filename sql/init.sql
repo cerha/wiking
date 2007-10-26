031dce230a65cb83f7f15fc2bdf2cce00eb558e1
cerha <cerha>
2007-10-26 17:26:43 +0000

    New `Olive' theme added.

--- sql/init.sql
+++ sql/init.sql
@@ -10,7 +10,8 @@ UPDATE pages SET _content='Your new Wiking site has been succesfully set up.\n\n
 WHERE mapping_id=1 AND lang='en';
 UPDATE pages SET content=_content;
 
-INSERT INTO config (site_title) VALUES ('Wiking site');
+--INSERT INTO themes ("name") VALUES ('Default');
+INSERT INTO config (site_title, theme) VALUES ('Wiking site');
 
 INSERT INTO stylesheets (identifier) VALUES ('default.css');
 
@@ -18,12 +19,24 @@ INSERT INTO users (login, password, firstname, surname, nickname, user_,
                    email, enabled, admin)
 VALUES ('admin', 'wiking', 'Wiking', 'Admin', 'Admin', 'Admin', '-', 't', 't');
 
-INSERT INTO themes ("name", foreground, background, border, heading_fg, heading_bg, heading_line, frame_fg,
-    frame_bg, frame_border, link, link_visited, link_hover, meta_fg, meta_bg, help, error_fg,
-    error_bg, error_border, message_fg, message_bg, message_border, table_cell, table_cell2,
-    button_fg, button, button_border, button_inactive_fg, button_inactive, button_inactive_border,
-    top_fg, top_bg, top_border, highlight_bg, inactive_folder, button_hover)
+INSERT INTO themes ("name", foreground, background, border, heading_fg, heading_bg, heading_line,
+    frame_fg, frame_bg, frame_border, link, link_visited, link_hover, meta_fg, meta_bg, help,
+    error_fg, error_bg, error_border, message_fg, message_bg, message_border,
+    table_cell, table_cell2, button_fg, button, button_border, button_hover,
+    button_inactive_fg, button_inactive, button_inactive_border,
+    top_fg, top_bg, top_border, highlight_bg, inactive_folder)
 VALUES ('Yellowstone', '#000', '#fff9ec', '#eda', '#420', '#fff0b0', '#eca', '#000', '#fff0d4',
         '#ffde90', '#a30', '#a30', '#f40', NULL, NULL, '#553', NULL, NULL, NULL, NULL, NULL, NULL,
-        '#fff', '#fff8f0', NULL, '#fe8', '#db9', NULL, '#ccc', '#999', '#444', '#fff', '#db9',
-        '#fb7', '#ed9', '#fd4');
+        '#fff', '#fff8f0', NULL, '#fe8', '#db9', '#fd4', NULL, '#ccc', '#999', '#444', '#fff',
+	'#db9', '#fb7', '#ed9');
+
+INSERT INTO themes ("name", foreground, background, border, heading_fg, heading_bg, heading_line, 
+    frame_fg, frame_bg, frame_border, link, link_visited, link_hover, meta_fg, meta_bg, help, 
+    error_fg, error_bg, error_border, message_fg, message_bg, message_border, 
+    table_cell, table_cell2, button_fg, button, button_border, button_hover,
+    button_inactive_fg, button_inactive, button_inactive_border,
+    top_fg, top_bg, top_border, highlight_bg, inactive_folder)
+VALUES ('Olive', '#000', '#fff', '#bcb', '#0b4a44', '#d2e0d8', NULL, '#000', '#e8eee8', '#d0d7d0',
+        '#042', NULL, '#d72', NULL, NULL, NULL, NULL, '#fc9', '#fa8', NULL, '#dfd', '#aea',
+        '#f8fbfa', '#f1f3f2', NULL, NULL, '#8a9', '#dff8df', NULL, '#ccc', '#999', NULL, '#efebe7',
+        '#8a9', '#fc8', '#d2e0d8');
