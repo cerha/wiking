c8fc7ee056a79c730c679c16c20cec84c7a0f8ad
cerha <cerha>
2008-10-29 16:41:03 +0000

    Version set to 0.4.5-pre3.

--- lib/wiking/__init__.py
+++ lib/wiking/__init__.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-__version__ = '0.4.5-pre2'
+__version__ = '0.4.5-pre3'
 
 import sys, os, time, string, re, copy, urllib
 
