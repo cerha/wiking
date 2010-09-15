00cad3a991465ae521c4582357180e16eea68e9e
Tomas Cerha <cerha@brailcom.org>
2010-09-15 15:30:51 +0200

    Make `ConfirmationDialog' work with latest LCG version.

--- lib/wiking/util.py
+++ lib/wiking/util.py
@@ -1336,6 +1336,9 @@ class LoginDialog(lcg.Content):
 class ConfirmationDialog(lcg.Container):
     """Dialog displaying arbitrary content followed by a `Continue' button."""
     
+    def _export_element_type(self):
+        return lcg.Container
+    
     def export(self, context):
         g = context.generator()
         return g.div((super(ConfirmationDialog, self).export(context),
