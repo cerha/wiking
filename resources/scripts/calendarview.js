730d91dc8c154e81c8934fa51d655dd48c888289
Tomas Cerha <cerha@brailcom.org>
2012-02-08 21:56:22 +0100

    Fix first calendar widget display when the date is empty

--- resources/scripts/calendarview.js
+++ resources/scripts/calendarview.js
@@ -4,7 +4,7 @@
 //
 // Maintained by Justin Mecham <justin@aspect.net>
 //
-// Portions Copyright 2002-2005, 2009, 2011 Mihai Bazon
+// Portions Copyright 2002-2005, 2009, 2011, 2012 Mihai Bazon
 //
 // This calendar is based very loosely on the Dynarch Calendar in that it was
 // used as a base, but completely gutted and more or less rewritten in place
@@ -457,7 +457,7 @@ Calendar.prototype = {
     this.container.appendChild(table)
 
     // Initialize Calendar
-    //this.update(this.date)
+    this.update(this.date)
 
     // Observe the container for mousedown events
     Event.observe(this.container, 'mousedown', Calendar.handleMouseDownEvent)
@@ -596,33 +596,6 @@ Calendar.prototype = {
 
 // global object that remembers the calendar
 window._popupCalendar = null
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //==============================================================================
