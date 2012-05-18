648b36f8242e2cd0a75c4ec68f8aa3b8a1255dc7
Milan Zamazal <pdm@brailcom.org>
2012-05-18 14:36:27 +0200

    New cache table a_user_roles

---
+++ sql/upgrade.36.sql
@@ -0,0 +1,29 @@
+create table a_user_roles (
+       uid int not null references users on update cascade on delete cascade,
+       role_id name not null references roles on update cascade on delete cascade
+);
+create index a_user_roles_uid_index on a_user_roles (uid);
+grant all on a_user_roles to "www-data";
+
+create or replace function update_user_roles () returns trigger as $$
+declare
+  uid_ int;
+  role_id_ name;
+begin
+  truncate a_user_roles;
+  for uid_ in select uid from users loop
+    for role_id_ in select role_id from role_members where uid=uid_ union select unnest(array['anyone', 'authenticated']) loop
+      insert into a_user_roles (select uid_, * from expanded_role(role_id_));
+    end loop;
+  end loop;
+  return null;
+end;
+$$ language plpgsql;
+
+create trigger role_sets_update_user_roles_trigger after insert or update or delete on role_sets
+for each statement execute procedure update_user_roles ();
+create trigger role_members_update_user_roles_trigger after insert or update or delete on role_members
+for each statement execute procedure update_user_roles ();
+
+-- Force triggers
+update role_sets set role_id='admin' where role_id='admin';
