3c12dd6532eae2f9589b1fc5004253ffc30a32b8
Tomas Cerha <cerha@brailcom.org>
2012-06-01 21:39:08 +0200

    Fix definition of cms_v_page_attachments

---
+++ sql/upgrade.37.sql
@@ -0,0 +1,10 @@
+create or replace view cms_v_page_attachments
+as select a.attachment_id  ||'.'|| l.lang as attachment_key, l.lang,
+  a.attachment_id, a.page_id, t.title, t.description,
+  a.filename, a.mime_type, a.bytesize,
+  a.image, a.thumbnail, a.thumbnail_size, a.thumbnail_width, a.thumbnail_height,
+  a.in_gallery, a.listed, a.author, a."location", a.width, a.height, a."timestamp"
+from cms_page_attachments a cross join cms_languages l
+     left outer join cms_page_attachment_texts t using (attachment_id, lang);
+
+update cms_database_version set version=37;
--- sql/wiking.sql
+++ sql/wiking.sql
@@ -374,12 +374,12 @@ create table cms_page_attachment_texts (
 );
 
 create or replace view cms_v_page_attachments
-as select a.attachment_id  ||'.'|| t.lang as attachment_key, t.lang,
+as select a.attachment_id  ||'.'|| l.lang as attachment_key, l.lang,
   a.attachment_id, a.page_id, t.title, t.description,
   a.filename, a.mime_type, a.bytesize,
   a.image, a.thumbnail, a.thumbnail_size, a.thumbnail_width, a.thumbnail_height,
   a.in_gallery, a.listed, a.author, a."location", a.width, a.height, a."timestamp"
-from cms_page_attachments a cross join cms_languages
+from cms_page_attachments a cross join cms_languages l
      left outer join cms_page_attachment_texts t using (attachment_id, lang);
 
 create or replace rule cms_v_page_attachments_insert as
