e41fe29fb8d12bf0ba5ec7ef6653d5e25f3e6e1c
Milan Zamazal <pdm@brailcom.org>
2012-11-20 15:32:21 +0100

    Fix of expanded_role for PostgreSQL 9.1

---
+++ sql/upgrade.40.sql
@@ -0,0 +1,12 @@
+drop function expanded_role(role_id name);
+create or replace function expanded_role (role_id_ name) returns setof name as $$
+declare
+  row record;
+begin
+  return next role_id_;
+  for row in select member_role_id from role_sets where role_sets.role_id=role_id_ loop
+    return query select * from expanded_role (row.member_role_id);
+  end loop;
+  return;
+end;
+$$ language plpgsql stable;
