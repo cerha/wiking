6122b3d538f93f59f4b5daad552cf06e118edf89
Tomas Cerha <cerha@brailcom.org>
2013-10-21 22:02:49 +0200

    Further improve/fix the last upgrade script

--- upgrade/upgrade.53.sql
+++ upgrade/upgrade.53.sql
@@ -5,6 +5,9 @@ declare
   parent cms_pages;
 begin
   select * into page from cms_pages where page_id=page_id_;
+  if page.kind = 'publication' then
+     return page.page_id;
+  end if;
   if page.kind != 'chapter' then
      return null;
   end if;
@@ -21,13 +24,20 @@ $$;
 
 -- Rename duplicate attachments (same file in multiple chapters) as we can not delete
 -- them here because this would leave stray files on the disk.
-update cms_page_attachments a set filename = 'xxx-' || page_id::text || '-' || filename
-from (select a.attachment_id
-      from cms_page_attachments a
-      join cms_page_attachments b
-      on a.filename=b.filename
-      and _parent_publication(a.page_id) in (_parent_publication(b.page_id), b.page_id)
+update cms_page_attachments a set filename = 'xxx-' || x.rank::text || '-' || filename
+from (
+select distinct a.attachment_id, 
+       rank() OVER (PARTITION BY a.filename||'::'||_parent_publication(a.page_id) ORDER BY a.page_id)
+from cms_page_attachments a
+join cms_page_attachments b
+on a.filename = b.filename
+and a.attachment_id != b.attachment_id
+and _parent_publication(a.page_id) is not null
+and _parent_publication(a.page_id) = _parent_publication(b.page_id)
 ) as x
-where a.attachment_id=x.attachment_id;
+where a.attachment_id=x.attachment_id and x.rank!=1;
+
+update cms_page_attachments set page_id = _parent_publication(page_id)
+where _parent_publication(page_id) is not null and page_id != _parent_publication(page_id);
 
 drop FUNCTION _parent_publication(page_id_ INTEGER);
