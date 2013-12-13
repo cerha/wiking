89939751a8462f4b6e18eadd17ae596c870400fc
Tomas Cerha <cerha@brailcom.org>
2013-12-13 11:49:18 +0100

    Make sure attachment parent page is considered in publication chapters
    Attachment of publication chapters are not bound through a binding column so
    the parent page didn't get to the record resolution condition, which lead to
    very strange effect, where another attachment of the same filename might have
    been selected on attachment access through referer URI.

--- lib/wiking/cms/cms.py
+++ lib/wiking/cms/cms.py
@@ -2916,6 +2916,16 @@ class Attachments(ContentManagementModule):
             else:
                 return None
         return super(Attachments, self)._tooltip_provider(req, uri, record, cid)
+        
+    def _binding_column(self, req):
+        column, value = super(Attachments, self)._binding_column(req)
+        if not column:
+            # If the parent page is not the in binding column, it is in the 
+            # binding prefill, see PublicationChapters.Spec.bindings... 
+            fw = self._binding_forward(req)
+            binding, record = fw.arg('binding'), fw.arg('record') 
+            column, value = 'page_id', binding.prefill()(record)['page_id'].value()
+        return column, value
 
     def _save_attachment_file(self, record):
         storage = wiking.cms.cfg.storage
