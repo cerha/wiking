21a8775b4d215a5a5c9bbae3df4d0075cf03d6f1
Tomas Cerha <cerha@brailcom.org>
2015-07-15 10:42:29 +0200

    Fix cms_f_user_roles for the case of user with no explicit roles

--- lib/wiking/dbdefs/sql/cms_f_all_user_roles.sql
+++ lib/wiking/dbdefs/sql/cms_f_all_user_roles.sql
@@ -1,1 +1,4 @@
-select role_id from a_user_roles where ($1 is null and uid is null) or ($1 is not null and uid=$1);
+-- Union is important for users, who have no explicit roles in a_user_roles. 
+select role_id from a_user_roles where uid is null
+union
+select role_id from a_user_roles where uid=$1;
