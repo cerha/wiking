c6e4fb9b66c1814c4b8041add0b4345969950787
Tomas Cerha <cerha@brailcom.org>
2015-09-04 12:39:08 +0200

    Improve presentation of configuration options overview in the documentation

--- doc/src/admin/config.py
+++ doc/src/admin/config.py
@@ -20,12 +20,22 @@ class Reader(lcg.Reader):
             content.append(lcg.PreformattedText(option.default_string()))
             return content
         import lcg
+        # Initial text is taken from configuration module docstring.
+        intro = lcg.Parser().parse(lcg.unindent_docstring(wiking.cfg.__doc__))
         # Construct sections according to module and subsections with the config options
-        return lcg.Parser().parse(lcg.unindent_docstring(wiking.cfg.__doc__)) + \
-            [lcg.TableOfContents(title="Available options in Wiking", depth=2)] + \
-            [lcg.Section(title=title,
-                         content=[lcg.Section(title="Option '%s': %s" % (o.name(),o.description()),
-                                              id=o.name(),
-                                              content=descr(o))
-                                  for o in cfg.options(sort=True) if o.visible()])
-             for title, cfg in [("Wiking", wiking.cfg), ("Wiking CMS", wiking.cms.cfg)]]
+        sections = [
+            lcg.Section(title=title, content=[
+                lcg.Section(title="Option '%s'" % o.name(),
+                            id=o.name(), descr=o.description(),
+                            content=descr(o))
+                for o in cfg.options(sort=True) if o.visible()])
+            for title, cfg in (("Wiking Configuration Options", wiking.cfg),
+                               ("Wiking CMS Configuration Options", wiking.cms.cfg))
+        ]
+        overview = [
+            lcg.Section(title=section.title() + " Overview", content=lcg.ul(
+                lcg.coerce(("Option '", lcg.link(s, s.id()), "': ", s.descr()))
+                for s in section.sections()))
+            for section in sections
+        ]
+        return intro + overview + sections
