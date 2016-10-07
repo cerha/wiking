9d7d3f50919aeaa3a9266b125a980bbfae5b84bf
Tomas Cerha <cerha@brailcom.org>
2016-10-07 08:46:48 +0200

    Release version 2.1.0

--- lib/wiking/__init__.py
+++ lib/wiking/__init__.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-__version__ = '2.0.0'
+__version__ = '2.1.0'
 
 import sys, os, time, string, re, copy, urllib, imp
 
