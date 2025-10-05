3932b23d1f47b21d5c06c3e6d23d51b23bc5168d
Tomáš Cerha <t.cerha@gmail.com>
2025-10-05 20:45:05 +0200

    Start 2.3.0.dev0

--- wiking/__init__.py
+++ wiking/__init__.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '2.2.1'
+__version__ = '2.3.0.dev0'
 
 from .util import (  # noqa: F401
     DBG, EVT, OPR,
