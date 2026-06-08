d994fc762c7f5f179d84917fd4a0a4767d1fd1de
Tomáš Cerha <cerha@truecode.cz>
2026-06-08 15:43:23 +0200

    Release 2.3.0

--- wiking/__init__.py
+++ wiking/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2006-2016 OUI Technology Ltd.
-# Copyright (C) 2019-2025 Tomáš Cerha <cerha@truecode.cz>
+# Copyright (C) 2019-2026 Tomáš Cerha <cerha@truecode.cz>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '2.3.0.dev0'
+__version__ = '2.3.0'
 
 from .util import (  # noqa: F401
     DBG, EVT, OPR,
